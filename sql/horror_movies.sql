541f30687aa07a92ce377345c8f28d724f428339
argearhart <117050870+argearhart@users.noreply.github.com>
2024-01-27 15:03:15 -0500

    Initial commit to add sql code

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,7 @@
--- Add your SQL here
+SELECT id AS Movie_ID, name AS Movie_Title, imdb_rating AS Rating
+FROM movies
+WHERE year <= 1985
+    AND genre = 'horror'
+ORDER BY imdb_rating DESC 
+LIMIT 3;
+   
